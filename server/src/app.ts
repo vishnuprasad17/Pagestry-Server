38c739141750833256736861d656a78339e43228
vishnuprasad17 <vishnu77781annur@gmail.com>
2026-02-08 22:55:21 +0530

    enabled cookie in CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ app.use(cors({
     origin: ['https://pagestryonline.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
 app.use('/api', routes);
